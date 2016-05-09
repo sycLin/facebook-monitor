c81ac5468f4cde1df23c2c79489e17a9d3bdd7c0
sycLin <b01902044@ntu.edu.tw>
2016-05-09 21:45:42 +0800

    +/- remove, trim, comment out some debug msg

--- background.js
+++ background.js
@@ -56,62 +56,51 @@ function fb_stop() {
 
 chrome.tabs.onCreated.addListener(function(tab) {
 	// check if the tab created is FB-related
-	console.log('a tab is created.');
+	// console.log('a tab is created.');
 	if(tab.url.indexOf("www.facebook.com") > -1) {
-		console.log('it is a facebook-related one.');
+		// console.log('it is a facebook-related one.');
 		// check if the tab is selected
 		if(tab.selected) {
 			// start the stopwatch
-			console.log('we should start the stopwatch.');
 			fb_start();
-		} else {
-			console.log('it is not selected.');
 		}
-	} else {
-		console.log('it is not a facebook related one.');
 	}
-	console.log("");
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	console.log('a tab is updated.');
+	// console.log('a tab is updated.');
 	// check the url
 	if(tab.url.indexOf("www.facebook.com") > -1) {
-		console.log('it is a facebook related one.');
+		//console.log('it is a facebook related one.');
 		// check if the tab is selected
 		if(tab.selected) {
 			// start the stopwatch
-			console.log('we should start the stopwatch.');
 			fb_start();
-		} else {
-			console.log('it is not selected.');
 		}
-	} else {
-		console.log('it is not a facebook related one.');
 	}
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	console.log('a tab is activated.');
+	// console.log('a tab is activated.');
 	// get the information about this activated tab
 	chrome.tabs.get(activeInfo.tabId, function(tab) {
 		// check the url
 		if(tab.url.indexOf("www.facebook.com") > -1) {
-			console.log('it is a facebook related one.');
-			// we should start the stopwatch
+			// console.log('it is a facebook related one.');
+			// start the stopwatch
 			fb_start();
 		} else {
-			console.log('it is not a facebook related one.');
-			// we should stop the stopwatch
+			// console.log('it is not a facebook related one.');
+			// stop the stopwatch
 			fb_stop();
 		}
 	});
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	console.log("a tab is being removed. (tabId = " + tabId + ")");
+	// console.log("a tab is being removed. (tabId = " + tabId + ")");
 	if(removeInfo.isWindowClosing) {
-		console.log("the window is also closing. (windowId = " + removeInfo.windowId + ")");
+		// console.log("the window is also closing. (windowId = " + removeInfo.windowId + ")");
 		// stop the stopwatch
 		fb_stop();
 	}
